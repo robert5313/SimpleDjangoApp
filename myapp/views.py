d6a0a09fc26028d0082e926e6be8cb48f22c9f52
robert5313 <robert.kimaiyo@masterclass.co.ke>
2024-02-05 23:38:22 +0300

    fixed about page

--- myapp/views.py
+++ myapp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
         return render(request, 'myapp/index.html')
+
+def about(request):
+        return render(request, 'myapp/about.html')
